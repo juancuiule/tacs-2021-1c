9771f58926e3437afbac5e48185c24069d99dda2
Mariano <sostomariano@gmail.com>
2021-04-18 23:18:26 -0300

    change

--- src/main/scala/com/utn/tacs/domain/match/MatchService.scala
+++ src/main/scala/com/utn/tacs/domain/match/MatchService.scala
@@ -1,8 +1,21 @@
 package com.utn.tacs.domain.`match`
 
-object MatchService {
-  def getMatch(matchId : String) : Option[Match] = {
+import cats.effect.IO
+import org.http4s.dsl.impl.UUIDVar
+
+object MatchService[F[Match]] {
+  def getMatch(matchId: String): Option[Match] = {
     val matchVal = new Match(matchId, "pepe", "roberto", "marvel_heroes", "active", "")
     return Some(matchVal)
+  }
+
+  def createMatch(player1Id: String, player2Id: String, deckId: String): F[Match] = {
+    //TODO: validar que los playerID existan y  que deckId exista
+    IO.pure(Match("", player1Id, player2Id, deckId, "active", ""))
+  }
+
+  def withdraw(matchId: String, playerId: String): Unit = {
+    //TODO:implementar
+    println(s"player: ${playerId} withdraws the match: ${matchId}")
   }
 }
--- src/main/scala/com/utn/tacs/infrastructure/endpoint/MatchEndpoints.scala
+++ src/main/scala/com/utn/tacs/infrastructure/endpoint/MatchEndpoints.scala
@@ -2,12 +2,15 @@ package com.utn.tacs.infrastructure.endpoint
 
 import cats.effect.Sync
 import cats.implicits._
-import com.utn.tacs.domain.`match`.MatchService
+import com.utn.tacs.domain.`match`.{Match, MatchService}
+import com.utn.tacs.domain.auth.Auth
 import io.circe.generic.auto._
 import io.circe.syntax._
-import org.http4s.HttpRoutes
+import org.http4s.{EntityDecoder, HttpRoutes}
 import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
+
+import scala.util.Try
 
 
 /*
@@ -16,32 +19,31 @@ import org.http4s.dsl.Http4sDsl
   * ??? /             <- continuar ???
   * GET /:id          <- traer partida
   * GET /:id/replay   <- traer jugadas y resultado de la partida
-  * ??? /:id/withdraw <- abandonar
+  * PUT /:id/withdraw <- abandonar
  */
 
 
 object MatchEndpoints {
 
   def decksRoutes[F[_] : Sync](): HttpRoutes[F] = {
+    implicit def decoder[F[_]: Sync]: EntityDecoder[F, Match] = jsonOf[F, Match]
     val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] {
       case GET -> Root / matchId =>
-        val maybeMatch = MatchService.getMatch(matchId)
-        maybeMatch
+        MatchService.getMatch(matchId)
           .map(m => Ok(m.asJson))
           .getOrElse(NotFound(s"match:${matchId} not found"))
-        /*val je : Match = new Match(matchId,"","","","","")
-          Ok(je.asJson)
-          */
-      case DELETE -> Root / id =>
+      case req@POST -> Root =>
         for {
-          resp <- Ok(s"deleted deck with id: ${id}")
+          payload  <- req.as[Match]
+          newMatch <- MatchService.createMatch(payload.player1Id, payload.player2Id, payload.deckId)
+          resp     <- Created(newMatch.asJson)
         } yield resp
-      case PATCH -> Root / id =>
-        for {
-          resp <- Ok(s"updated deck with id: ${id}")
-        } yield resp
+      case req@PUT -> Root / matchId / "withdraw" =>
+        val payload: Match = req[Match]
+        MatchService.withdraw(matchId, payload.player1Id)
+        Accepted(s"match: ${matchId} was withdrawn by player:${payload.player1Id}")
     }
   }
 }
