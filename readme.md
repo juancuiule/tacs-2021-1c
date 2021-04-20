d39f6955ac59ff38f7313a9c013502915e7d1044
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2021-04-20 18:15:50 -0300

    use cards api

--- readme.md
+++ readme.md
@@ -8,6 +8,7 @@
 ## Ejecutar la aplicación 📦
 
 * Para ejecutar la aplicación, es necesario tener [Docker](https://www.docker.com/) instalado
+* Crear un archivo `.env` en el root del proeycto que tenga la key de la api de superheros (hay un ejemplo en `.env.example`)
 * En el root del proyecto, ejecutar el comando `docker-compose up` 
 * Esperar hasta que se genere el build y corra el container
 * Cuando veas un mensaje similar a `http4s v0.21.16 on blaze v0.14.14 started at http://0.0.0.0:8080/`, el server está corriendo y podés acceder a la API con la URL [http://localhost:8080/](http://localhost:8080/)
--- src/main/scala/com/utn/tacs/infrastructure/endpoint/CardEndpoints.scala
+++ src/main/scala/com/utn/tacs/infrastructure/endpoint/CardEndpoints.scala
@@ -2,7 +2,8 @@ package com.utn.tacs.infrastructure.endpoint
 
 import cats.effect.Sync
 import cats.implicits._
-import com.utn.tacs.domain.cards.{Card, PowerStats} //, CardApiRequester}
+import com.utn.tacs.domain.cards.CardApiRequester
+
 import io.circe.Json
 import io.circe.generic.auto._
 import io.circe.syntax._
@@ -11,25 +12,21 @@ import org.http4s.circe._
 import org.http4s.dsl.Http4sDsl
 
 object CardEndpoints {
-  def cardsRoutes[F[_] : Sync](): HttpRoutes[F] = {
+  def cardsRoutes[F[_] : Sync](cardRequester: CardApiRequester[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F] {}
     import dsl._
-
-    val powerStats: PowerStats = PowerStats("10", "10", "10", "10", "10", "10")
-    val card: Card = Card("1", "batman", powerStats)
-    val cards: List[Card] = List(card, card, card)
 
     HttpRoutes.of[F] {
       case GET -> Root / id =>
         print(id)
         for {
-          //          card <- cardRequester.getById(id)
+          card <- cardRequester.getById(id)
           resp <- Ok(card.asJson)
         } yield resp
       case GET -> Root / "name" / name =>
         print(name)
         for {
-          //          cards <- cardRequester.getByName(name)
+          cards <- cardRequester.getByName(name)
           resp <- Ok(Json.obj(
             ("cards", cards.map(card => card.asJson).asJson)
           ))
