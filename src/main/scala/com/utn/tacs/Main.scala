ac3151957db0e0c171ae7b52fd741340877d06c5
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2021-06-08 17:10:34 -0300

    check if env var is empty

--- src/main/scala/com/utn/tacs/Main.scala
+++ src/main/scala/com/utn/tacs/Main.scala
@@ -7,11 +7,13 @@ import fs2.concurrent.Topic
 object Main extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
     scala.util.Properties.envOrNone("SUPERHERO_API_KEY") match {
-      case Some(_) => for {
+      case Some(apiKey) if apiKey != "" =>
+        println("Encontré la variable de entorno: SUPERHERO_API_KEY")
+        for {
         topic <- Topic[IO, OutputMessage](SendToUsers(Set.empty, None))
         exitCode <- Server.createServer[IO](topic).compile.drain.as(ExitCode.Success)
       } yield exitCode
-      case None => {
+      case _ => {
         println("Falta la variable de entorno: SUPERHERO_API_KEY")
         IO(ExitCode.Error)
       }
